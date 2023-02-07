9d19cd3c42941358fad25d5524aa2a38a8a0a740
andrewtran10 <andrewtran47@gmail.com>
2023-02-06 21:55:52 -0500

    create ReadyListener

---
+++ src/discord/listeners/ReadyListener.ts
@@ -0,0 +1,12 @@
+import { AntiDegenBot } from "../../main/AntiDegenBot";
+import { DiscordEventListener } from "../DiscordEventListener";
+import { Events } from "discord.js";
+
+
+export class ReadyListener extends DiscordEventListener {
+    registerListener(antiDegenbot: AntiDegenBot) : void {
+        antiDegenbot.on(Events.ClientReady as never, async () => {
+            if (antiDegenbot.user) console.log(`Ready! Logged in as ${antiDegenbot.user.tag}`);
+        });
+    };
+}
